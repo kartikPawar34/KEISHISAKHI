e3e2168c7b3493e2413a4d68b100e8ffc6e5b839
Kartik Pawar <kp346949@gmail.com>
2026-01-10 11:29:55 -0500

    Fix: Added base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from "path"
 
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+// https://vitejs.dev/config/
+export default defineConfig({
+  // This ensures the site looks for files in /KEISHISAKHI/ instead of the root
+  base: '/KEISHISAKHI/', 
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+})
